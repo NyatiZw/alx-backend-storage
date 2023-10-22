6791a6d6ba611cec9777e2068605f6d5fd743370
NyatiZw <appsmaid19@gmail.com>
2023-10-22 07:02:34 -0700

    List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""This is a script for listing all documents"""
+
+from pymongo.collection import Collection
+
+def list_all(mongo_collection: Collection):
+    """Lists all documents in the given collection
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): Collection
+
+    Returns:
+        list: List of documents
+    """
+    try:
+        documents = list(mongo_collection.find({}))
+        return documents
+    expect Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+
+if __name__ == "__main__":
+    main()       
