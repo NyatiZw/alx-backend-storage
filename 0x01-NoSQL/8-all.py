f5026d78c4f438d81d0fedd84c613bf67af4d3a5
NyatiZw <appsmaid19@gmail.com>
2023-10-22 07:07:45 -0700

    List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This is a script for listing all documents"""
 
-from pymongo.collection import Collection
+from pymongo import Collection
 
 def list_all(mongo_collection: Collection):
     """Lists all documents in the given collection
