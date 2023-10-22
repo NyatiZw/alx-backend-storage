2bd8f9f6f86e6798b6cd3b0ce686aa117ec795c0
NyatiZw <appsmaid19@gmail.com>
2023-10-22 08:33:17 -0700

    corrections

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,15 +2,16 @@
 """This is a script for listing all documents"""
 
 
-from pymongo.collection import Collection
+from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
-    try:
-        documents = list(mongo_collection.find({}))
-        return documents
-    except Exception as e:
-        return []
+    client = MongoClient("localhost", 27017, maxPoolSize=50)
+    db = client.localhost
+    collection = db['chain']
+    cursor = collection.find({})
+    for document in cursor:
+        print(document)
 
 
 if __name__ == '__main__':
