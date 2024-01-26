3bc4ba7777906f3a408e2be0abfac8b57ba3fa85
NyatiZw <appsmaid19@gmail.com>
2024-01-26 20:42:22 +0000

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -89,4 +89,15 @@ def count_calls(method: Callable):
 
 
 if __name__ == "__main__":
-    main()
+    # Test cases
+    cache = Cache()
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
